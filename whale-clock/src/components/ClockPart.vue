ded24d58aead87ba5786dd1859a12fd013720a35
KoreanJungYJ <jung22337@naver.com>
2018-01-28 22:41:35 +0900

    시계 로직

--- whale-clock/src/components/ClockPart.vue
+++ whale-clock/src/components/ClockPart.vue
@@ -47,6 +47,7 @@ export default {
       hours: new Date().getHours(),
       minutes: new Date().getMinutes(),
       seconds: new Date().getSeconds(),
+      millis: new Date().getMilliseconds(),
 
       timeContainer: [
         {
@@ -80,12 +81,28 @@ export default {
     }
   },
   computed: {
+    correctHour () {
+      let hour = this.hours
+
+      if (hour >= 12) {
+        hour = hour - 12
+      }
+      if (hour === 0) {
+        hour = 12
+      }
+
+      return hour
+    },
     getAngle () {
       const angles = [
-        ((this.hours * 30) + (this.minutes / 2)),
+        // 현재 시
+        ((this.correctHour * 30) + (this.minutes / 2)),
+        // 현재 분
         (this.minutes * 6),
-        (this.seconds * 6)
+        // 현재 초
+        ((this.seconds * 6) + (this.millis / 1000))
       ]
+
       return angles
     },
     morning () {
@@ -99,25 +116,6 @@ export default {
     }
   },
   methods: {
-    clockOperating () {
-      this.initClock()
-      this.setupMinute()
-      this.moveMinute()
-      this.moveSecond()
-    },
-    initClock () {
-      let tc = this.$refs['time-container']
-      for (let i in tc) {
-        tc[i].style.transform = `rotateZ(${this.getAngle[i]}deg)`
-        tc[i].style.webkitTransform = `rotateZ(${this.getAngle[i]}deg)`
-      }
-    },
-    setupMinute () {
-    },
-    moveMinute () {
-    },
-    moveSecond () {
-    },
     search () {
       let typeMessage = this.message
       const searchUrl = `https://www.google.co.kr/search\
@@ -125,10 +123,21 @@ export default {
 60l5.1198j0j7&sourceid=chrome&ie=UTF-8`
 
       window.location.href = searchUrl
+    },
+    getTime () {
+      let self = this
+
+      setInterval(function () {
+        let timeContainer = self.$refs['time-container']
+        for (let i = 0; i < timeContainer.length; i++) {
+          timeContainer[i].style.transform = `rotateZ(${self.getAngle[i]}deg)`
+          timeContainer[i].style.webkitTransform = `rotateZ(${self.getAngle[i]}deg)`
+        }
+      }, 50)
     }
   },
   mounted () {
-    this.clockOperating()
+    this.getTime()
   }
 }
 </script>
@@ -221,28 +230,28 @@ export default {
     justify-content: center;
   }
 
-  .hours-container {
+  /*.hours-container {
     animation: rotate 43200s infinite linear;
   }
 
   .minutes-container {
     animation: rotate 3600s infinite linear;
-  }
+  }*/
 
   /* 째깍거리는 느낌을 주고 싶다면 linear대신 steps(60)으로 주자! */
   /* 째깍거림과 동시에 뭔가 튀는 느낌을 주고 싶다면 cubic-bezier를 이용하자 */
   /* 예시: transition: transform 0.2s cubic-bezier(.4, 2.08, .55, .44); */
 
-  .seconds-container {
+  /*.seconds-container {
     animation: rotate 60s infinite linear;
-  }
+  }*/
 
   /* Rotate Animation */
-  @keyframes rotate {
+  /*@keyframes rotate {
     100% {
       transform: rotateZ(360deg);
     }
-  }
+  }*/
 
   .seconds {
     width: 2px;
